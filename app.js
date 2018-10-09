4d9d3d0bb5fb262628a769fbf3afe72e2df63792
Alvin <alvinsim74@gmail.com>
2018-10-09 20:24:27 +0800

    updated index.html

---
+++ app.js
@@ -0,0 +1,17 @@
+$(function() {
+                
+    var documentEl = $(document),
+        fadeElem = $('.slider');
+    
+    
+    documentEl.on('scroll', function() {
+        var currScrollPos = documentEl.scrollTop();
+        
+        fadeElem.each(function() {
+            var $this = $(this),
+                elemOffsetTop = $this.offset().top;
+            if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/400);
+        }); 
+    });
+    
+});
