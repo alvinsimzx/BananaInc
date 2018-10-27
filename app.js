2aeb4a40f1507fafc165b45647b88f31b0d3fece
Alvin <alvinsim74@gmail.com>
2018-10-27 21:29:45 +0800

    improved slideshow with javascript

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
-$(function() {
-                
-    var documentEl = $(document),
-        fadeElem = $('.slider');
-    
-    
-    documentEl.on('scroll', function() {
-        var currScrollPos = documentEl.scrollTop();
-        
-        fadeElem.each(function() {
-            var $this = $(this),
-                elemOffsetTop = $this.offset().top;
-            if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/400);
-        }); 
-    });
-    
+let sliderImages = document.querySelectorAll('.slide'),
+arrowleft = document.querySelector('#arrow-left'),
+arrowright = document.querySelector('#arrow-right'),
+current=0;
+
+
+//clear all images
+function reset(){
+    for(let i = 0; i< sliderImages.length; i++){
+        sliderImages[i].style.display='none';
+    }
+}
+
+//init slider
+function startSlide(){
+    reset();
+    sliderImages[0].style.display='block';
+}
+
+//show previous
+function slideleft(){
+    reset()
+    sliderImages[current-1].style.display='block';
+    current--;
+
+}
+
+//show next
+function slideright(){
+    reset()
+    sliderImages[current+1].style.display='block';
+    current++;
+}
+
+arrowright.addEventListener('click',function(){
+    if(current==sliderImages.length-1){
+        current=-1;
+    }
+    slideright();
 });
+
+
+
+//left arrow click
+arrowleft.addEventListener('click',function(){
+    if(current==0){
+        current=sliderImages.length;
+    }
+    slideleft();
+});
+
+startSlide()
